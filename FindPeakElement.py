9d21653e5ea4bc895995bf059fcc0f8ce9f7b7fc
unknown <irenelie1412@gmail.com>
2022-07-09 01:36:01 +0800

    Update FindPeakElement.py

--- FindPeakElement.py
+++ FindPeakElement.py
@@ -4,26 +4,19 @@ from typing import List
 class Solution:
     def findPeakElement(self, nums: List[int]) -> int:
         
-        def binarySearch(start, end):
-            if start == end:
-                print('return ' + str(start))
-                return start
-
-            mid = (start + end) // 2
-            print('when num is ' + str(nums[mid]))
+        def binarySearch(l:int, r: int) -> int:
+            if l == r:
+                return l
             
+            mid = (l + r) // 2
+            
+            if nums[mid] > nums[mid + 1]:
+                return binarySearch(l, mid)
+            
+            return binarySearch(mid + 1, r)
         
-            
-            if mid - 1 >= 0 and nums[mid - 1] > nums[mid]:
-                print('left number is bigger than mid')
-                binarySearch(start, mid)
-            
-            elif mid + 1 < len(nums) and nums[mid + 1] > nums[mid]:
-                print('right number is bigger than mid')
-                binarySearch(mid, end)
-
-        peak = binarySearch(0, len(nums) - 1)   
-        return peak
+        return binarySearch(0, len(nums) - 1)
+    
 
 sol = Solution()
 nums = [1,2,3,1]
