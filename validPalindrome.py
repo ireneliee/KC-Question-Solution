6f14236c84e1032d466ff696471483fbdb764086
unknown <irenelie1412@gmail.com>
2022-07-29 02:10:33 +0800

    Create validPalindrome.py

---
+++ validPalindrome.py
@@ -0,0 +1,23 @@
+from collections import deque
+
+
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        stack = deque()
+        ori = ""
+        for i in range(len(s)):
+            curr = s[i].lower()
+            if 97 <= ord(curr) <= 122 or 48 <= ord(curr) <= 57:
+                ori = ori + curr
+
+
+
+        for i in range(len(ori)):
+            if ori[i] != ori[len(ori)- 1 - i]:
+                return False
+        return True
+
+sol = Solution()
+word = "race a car"
+print(sol.isPalindrome(word))
+
