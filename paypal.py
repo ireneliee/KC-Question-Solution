cae143e00bcd5197b622abd58346888cf5a5574e
unknown <irenelie1412@gmail.com>
2022-09-02 23:53:19 +0800

    subsetA

---
+++ paypal.py
@@ -0,0 +1,35 @@
+import math
+
+def getMostVisited(n, sprints):
+    run_map = {}
+    for i in range(1, n + 1):
+        run_map[i] = 0
+    
+    for i in range(len(sprints) - 1):
+        start = sprints[i]
+        end = sprints[i + 1]
+
+        if start < end:
+            for j in range(start, end + 1):
+                
+                curr = run_map[j]
+                run_map[j] = curr + 1
+        elif start > end:
+            j = start
+            while j >= end:
+                
+                curr = run_map[j]
+                run_map[j] = curr + 1
+                j = j - 1
+
+    curr_max = 0
+    result = 0
+    for i in range(1, n + 1):
+        curr = run_map[i]
+        if curr > curr_max:
+            curr_max = curr
+            result = i
+    return result
+
+
+print(getMostVisited(5,[2,4,1,3]))
