ff6054ba7c67ecb2329d758b59752173f18fedc5
Irene Lie <irenelie1412@gmail.com>
2023-07-13 14:30:48 +0800

    Update uniquePath.py

--- uniquePath.py
+++ uniquePath.py
@@ -1,18 +1,17 @@
 class Solution:
-    def uniquePaths(self, m: int, n: int) -> int:
-
-        row = [1] * n
-
-        for i in range(m - 1):
-            newRow = [1] * n
-            for j in range(n - 2, -1, -1):
-                newRow[j] = newRow[j + 1] + row[j]
-            row = newRow
-
-        return row[0] 
-        
-            
-        
-        
+    def uniquePaths(self, m, n):
+        count_dict = {}
+        def find(currM, currN):
+            if (currM, currN) in count_dict:
+                return count_dict[(currM, currN)]
+            if currM >= m or currN >= n:
+                print('Pass here B')
+                return 0
+            if currM == m - 1 and currN == n - 1:
+                print('Pass here C')
+                return 1
+            count_dict[(currM, currN)] = find(currM + 1, currN) + find(currM, currN + 1)
+            return count_dict[(currM, currN)] 
+        return find(0, 0)
 sol = Solution()
 print(sol.uniquePaths(3, 7))
