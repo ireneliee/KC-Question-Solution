0cf7bca9fa63e850da0dbbc9e9c40c4c5017b94f
Irene Lie <77621546+ireneliee@users.noreply.github.com>
2023-08-13 22:02:08 +0800

    group anagram

---
+++ groupAnagram.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def groupAnagrams(self, strs):
+        word_dict = {}
+        for item in strs:
+            char_list = list(item)
+            char_list.sort()
+            char_tuple = tuple(char_list)
+
+            if char_tuple not in word_dict:
+                word_dict[char_tuple] = []
+
+            word_dict[char_tuple].append(item)
+        
+        print(word_dict)
+
+        result = list(word_dict.values())
+        print(result)
+
+s = Solution()
+strs = ["eat","tea","tan","ate","nat","bat"]
+print(s.groupAnagrams(strs))
